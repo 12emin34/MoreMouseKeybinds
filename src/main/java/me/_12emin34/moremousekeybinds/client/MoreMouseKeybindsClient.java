6e36f5575565ee18e369ac1849dd813ffaa16cde
12emin34 <macanovic.emin@gmail.com>
2022-10-11 22:58:56 +0200

    clear unused imports

--- src/main/java/me/_12emin34/moremousekeybinds/client/MoreMouseKeybindsClient.java
+++ src/main/java/me/_12emin34/moremousekeybinds/client/MoreMouseKeybindsClient.java
@@ -6,13 +6,9 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.GameVersion;
-import net.minecraft.SharedConstants;
-import net.minecraft.client.RunArgs;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.server.ServerMetadata;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
